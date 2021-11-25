a3e7e830514aab55418e232252a9a125736b97fa
sudheer <sudheerlikeu@gmail.com>
2021-11-25 13:43:43 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
+++ Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
@@ -2,6 +2,6 @@
 Multiple providers
 
 ->  we can define the multiple configuretion for same provider  and select which one to use on a per-resource or per modile basis.
-->  the primary reason to 
+->  the primary reason to support to 
 
 */
