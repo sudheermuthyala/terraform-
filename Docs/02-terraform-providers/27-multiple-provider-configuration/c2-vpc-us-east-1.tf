e5ed5dd4e4c16728f6ac14830911f6cfd8e2a3fb
sudheer <sudheerlikeu@gmail.com>
2021-11-25 14:19:20 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/27-multiple-provider-configuration/c2-vpc-us-east-1.tf
+++ Docs/02-terraform-providers/27-multiple-provider-configuration/c2-vpc-us-east-1.tf
@@ -1,6 +1,7 @@
-# resource block  to create vpc in us-west-1
+# resource block  to create vpc in us-east-1
 resource "aws_vpc" "my_us-east-1-vpc" {
   cidr_block = "10.0.0.0/16"
+  
   tags = {
     "Name" = "MY-VPC-us-east-1"
   }
