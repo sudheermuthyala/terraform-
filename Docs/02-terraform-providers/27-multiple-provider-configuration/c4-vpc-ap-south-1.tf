204d00c03d141cbe2ee9299c805d810454c03831
sudheer <sudheerlikeu@gmail.com>
2021-11-25 14:34:25 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/27-multiple-provider-configuration/c4-vpc-ap-south-1.tf
+++ Docs/02-terraform-providers/27-multiple-provider-configuration/c4-vpc-ap-south-1.tf
@@ -1,6 +1,10 @@
 # resouce to create vpc in ap-south-1
 
 resource "aws_vpc" "MY-Vpc-in-ap-south-1" {
-    cidr_block = "10.0.0.0"
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      "Name" = "value"
+    }
   
 }
