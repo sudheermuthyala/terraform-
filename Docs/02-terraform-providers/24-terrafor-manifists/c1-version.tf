ac86fed63b4611d92f9f61ab25012aeba90e2cc9
sudheer <sudheerlikeu@gmail.com>
2021-11-25 12:19:40 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
+++ Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
@@ -53,7 +53,10 @@ The AWS provider offers a flexible means of providing credentials for authentica
 - Shared credentials/configuration file
       You can use an AWS credentials or configuration file to specify your credentials. The default location is $HOME/.aws/credentials on Linux and macOS, or "%USERPROFILE%\.aws\credentials" on Windows. You can optionally specify a different location in the Terraform configuration by providing the shared_credentials_file argument or using the AWS_SHARED_CREDENTIALS_FILE environment variable. This method also supports a profile configuration and matching AWS_PROFILE environment variable:
       
-      1-  ~/.aws/credentials
+      1)  ~/.aws/credentials
+          [default]
+aws_access_key_id=AKIAIOSFODNN7EXAMPLE
+aws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 
 
       Usage:
