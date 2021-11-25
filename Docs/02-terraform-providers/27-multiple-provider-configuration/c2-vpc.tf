f049f41c5b4507b3dd16a42300ba9290691f4b9f
sudheer <sudheerlikeu@gmail.com>
2021-11-25 14:12:19 +0530

    --- Auto-Git Commit ---

---
+++ Docs/02-terraform-providers/27-multiple-provider-configuration/c2-vpc.tf
@@ -0,0 +1,28 @@
+#Terrafrom Block File
+terraform {
+  required_version = "1.0.11"
+  required_providers {
+    aws =   {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  } 
+}
+
+
+#Provider-1     for US-east-1 (default-provider)
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+  
+}
+
+
+#Provider-2     for US-west-1 (second provider)
+
+provider "aws" {
+    profile = "default"
+    region  = "us-west-1"
+    alias = "aws-west-1" 
+  
+}
