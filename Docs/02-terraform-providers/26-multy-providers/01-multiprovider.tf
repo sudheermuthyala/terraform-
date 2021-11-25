b46947be1af222921a980d4dc15484749f77f6dc
sudheer <sudheerlikeu@gmail.com>
2021-11-25 13:45:03 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
+++ Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
@@ -3,6 +3,6 @@ Multiple providers
 
 ->  we can define the multiple configuretion for same provider  and select which one to use on a per-resource or per modile basis.
 ->  the primary reason for this is to support multiple reason for a cloud platform
-->  we can ues alternate provider 
+->  we can ues alternate provider in a resource, data
 
 */
