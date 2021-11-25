077fe16253115789c9e2d65d258ee2a62bac2140
sudheer <sudheerlikeu@gmail.com>
2021-11-25 12:16:03 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
+++ Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
@@ -39,6 +39,8 @@ The AWS provider offers a flexible means of providing credentials for authentica
 
 - Environment variables
       You can provide your credentials via the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY, environment variables, representing your AWS Access Key and AWS Secret Key, respectively. Note that setting your AWS credentials using either these (or legacy) environment variables will override the use of AWS_SHARED_CREDENTIALS_FILE and AWS_PROFILE. The AWS_DEFAULT_REGION and AWS_SESSION_TOKEN environment variables are also used, if applicable:
+
+      Usage:
       
 - Shared credentials/configuration file
 - CodeBuild, ECS, and EKS Roles
