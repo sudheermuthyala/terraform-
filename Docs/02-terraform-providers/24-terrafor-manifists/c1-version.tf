4f14e7a09ccd27fd4fec84871d7ed0c3aabfcb00
sudheer <sudheerlikeu@gmail.com>
2021-11-25 12:13:22 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
+++ Docs/02-terraform-providers/24-terrafor-manifists/c1-version.tf
@@ -25,3 +25,10 @@ provider "aws" {
 }
 /*
 Authentication
+The AWS provider offers a flexible means of providing credentials for authentication. The following methods are supported, in this order, and explained below:
+
+Static credentials
+Environment variables
+Shared credentials/configuration file
+CodeBuild, ECS, and EKS Roles
+EC2 Instance Metadata Service (IMDS and IMDSv2)
