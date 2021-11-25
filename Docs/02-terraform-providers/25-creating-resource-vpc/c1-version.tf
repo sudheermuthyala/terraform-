2228a7ebebe4277959aff82a6d987e47a37fe43f
sudheer <sudheerlikeu@gmail.com>
2021-11-25 12:45:10 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/25-creating-resource-vpc/c1-version.tf
+++ Docs/02-terraform-providers/25-creating-resource-vpc/c1-version.tf
@@ -78,6 +78,6 @@ The AWS provider offers a flexible means of providing credentials for authentica
 #terraform.tfstate   
     
     terraform.tfstate  will creats while we creat any resource this terraform.tfstate ile is nothing but a
-    
+    terraform
 
 */
