9965f8b0b8e6c200d968f2daa6f747e5a193e8f4
sudheer <sudheerlikeu@gmail.com>
2021-11-25 10:06:53 +0530

    auto pushing chainges repo

--- Docs/21-terraform-block-provider/terraform-required-provider.tf
+++ Docs/21-terraform-block-provider/terraform-required-provider.tf
@@ -5,9 +5,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      #version = "~> 3.0"
+      version = "~> 3.0"
       #version = ">= 3.0.0, <= 3.1.0"
-      version = "~> 2.0"
+      #version = "~> 2.0"
     }
   }
 }
