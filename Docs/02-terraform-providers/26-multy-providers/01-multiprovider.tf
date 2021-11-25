88372c923c3b4afb53c6241c218ff9618537e29c
sudheer <sudheerlikeu@gmail.com>
2021-11-25 13:42:33 +0530

    --- Auto-Git Commit ---

--- Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
+++ Docs/02-terraform-providers/26-multy-providers/01-multiprovider.tf
@@ -1,6 +1,7 @@
 /*
 Multiple providers
 
-->  we can define the multiple configuretion for same provider  and select which one to use on a per-resource or per modiles
+->  we can define the multiple configuretion for same provider  and select which one to use on a per-resource or per modile basis.
+-
 
 */
