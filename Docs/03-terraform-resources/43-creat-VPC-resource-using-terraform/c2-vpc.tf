4bb380a588933e5e51f78ba4457b748a63a07114
sudheer <sudheerlikeu@gmail.com>
2021-11-26 17:10:41 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "MY_DEV_SG" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = []
+    cidr_blocks      = ["0.0.0.0/0"]
     
   }
 
