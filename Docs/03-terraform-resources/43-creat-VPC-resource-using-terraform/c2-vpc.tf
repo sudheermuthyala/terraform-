65788abd14dec6f0903b5d572640ddef643ed0bf
sudheer <sudheerlikeu@gmail.com>
2021-11-26 16:28:31 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -1,8 +1,11 @@
-# vpc resource block
+#   vpc resource block
 #   Resource-1: Create VPC
+
 resource "aws_vpc" "MY_DEV_VPC" {
     cidr_block = "10.0.0.0/16"
 
-
+    tags = {
+      "key" = "value"
+    }
   
 }
