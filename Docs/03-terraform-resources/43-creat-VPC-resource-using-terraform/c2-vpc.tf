b5c71bcc69e183c83c8c1fd4f3d0f99fe3971b11
sudheer <sudheerlikeu@gmail.com>
2021-11-26 16:53:44 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -34,6 +34,6 @@ resource "aws_route_table" "MY_DEV_ROUT_TABLE" {
 
 #   Resource-4: Create rout in rout Table for internet access
 
-resource "aws_route" "name" {
+resource "aws_route" "MY_DEV_ROUT_TABLE" {
   
 }
