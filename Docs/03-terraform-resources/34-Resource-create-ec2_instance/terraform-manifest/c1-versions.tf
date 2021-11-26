5212af0203aebde8ca094295294ff00e885f43ce
sudheer <sudheerlikeu@gmail.com>
2021-11-26 11:21:46 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/34-Resource-create-ec2_instance/terraform-manifest/c1-versions.tf
+++ Docs/03-terraform-resources/34-Resource-create-ec2_instance/terraform-manifest/c1-versions.tf
@@ -2,9 +2,9 @@
 terraform {
   required_version = "~> 1.0.11"
   required_providers {
-    aws     ={
-        source = "hashicorp/aws"
-        version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
@@ -12,7 +12,7 @@ terraform {
 # provider block
 
 provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-  
+  profile = "default"
+  region  = "us-east-1"
+
 }
