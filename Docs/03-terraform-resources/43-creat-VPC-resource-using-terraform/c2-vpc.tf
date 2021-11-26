f451aa92aba48fb582212b1b87731a2b42d5ec72
sudheer <sudheerlikeu@gmail.com>
2021-11-26 17:04:09 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -42,7 +42,7 @@ resource "aws_route" "MY_DEV_ROUT" {
 }
 #   - Resource-6: Associate Route Table with Subnet
 resource "aws_route_table_association" "MY_DEV_ROUT_TABLE_ASSOCIATION" {
-    route_table_id = 
+    route_table_id = aws_route_table.MY_DEV_ROUT_TABLE.i
   
 }
 #   - Resource-7: Create Security Group in the VPC with port 80, 22 as inbound open
