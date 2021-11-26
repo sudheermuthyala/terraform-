2d258d3dd782ca0a2b91fbb7bf3140ad00afdf88
sudheer <sudheerlikeu@gmail.com>
2021-11-26 16:36:21 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "MY_DEV_VPC" {
   
 }
 
+#   Resource-1: Create VPC
 resource "aws_subnet" "MY_DEV_VPC_PUBLIC_SUBNET-1" {
     vpc_id = aws_vpc.MY_DEV_VPC.id
     cidr_block = "10.0.1.0/24"
