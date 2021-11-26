a70b1271caa883ffb69a5a92ac88af7b3acbb205
sudheer <sudheerlikeu@gmail.com>
2021-11-26 11:15:44 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/34-Resource-create-ec2_instance/terraform-manifest/c2-ec2-instance.tf
+++ Docs/03-terraform-resources/34-Resource-create-ec2_instance/terraform-manifest/c2-ec2-instance.tf
@@ -2,5 +2,7 @@
 // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "MY_ec2_vm" {
-  ami = ""
+  ami                 = "ami-04902260ca3d33422"
+  availability_zone   = "us-east-1a"
+  instance_type       = "t2.micre"
 }
