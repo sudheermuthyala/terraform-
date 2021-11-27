ce3a4c416534602f1ecdf897404e72a1338470e7
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:10:38 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c2-vpc.tf
@@ -1,4 +1,11 @@
 #   vpc resource block
+#   Resource-1: Create VPC
+#   Resource-2: Create Subnets
+#   Resource-3: Create internet-gateway
+#   Resource-4: Create rout table
+#   Resource-5: Create rout in rout Table for internet access
+#   Resource-6: Associate Route Table with Subnet
+#   Resource-7: Create Security Group in the VPC with port 80, 22 as inbound open
 
 #   Resource-1: Create VPC
 resource "aws_vpc" "MY_DEV_VPC" {
