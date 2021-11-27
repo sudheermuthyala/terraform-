0538a866412000165e565ea35ec77579f9ab862e
sudheer <sudheerlikeu@gmail.com>
2021-11-27 10:05:36 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
@@ -5,5 +5,5 @@ resource "aws_instance" "MY_DEV_VM" {
     key_name = "terraform_k"
     subnet_id = aws_subnet.DEV_SUBNET.id
     vpc_security_group_ids = [ aws_security_group.DEV_PUB_SG.id ]
-    user_data = file()
+    user_data = file(apache-install.sh)
 }
