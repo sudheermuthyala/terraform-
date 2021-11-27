1e0b302e85148c8c4dd4c219706797a524a89165
sudheer <sudheerlikeu@gmail.com>
2021-11-27 10:07:15 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
@@ -8,11 +8,11 @@ resource "aws_instance" "MY_DEV_VM" {
     #user_data = file(apache-install.sh)
     user_data = <<-EOF
     #! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl  start  httpd
-sudo systemctl enable httpd
-echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl  start  httpd
+    sudo systemctl enable httpd
+    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
 
     EOF
 
