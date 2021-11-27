556f9859331c0a32d0412c341c421adbb4fb94ce
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:34:26 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
@@ -57,8 +57,9 @@ resource "aws_route_table_association" "DEV_R_T_ASSOCIATION" {
   subnet_id = aws_subnet.DEV_SUBNET.id
   
 }
+#   Resource-7: Create Security Group in the VPC with port 80, 22 as inbound open
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "DEV_PUB_" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
