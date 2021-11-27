87e41d26551618cf9a081e197781b65402074d50
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:30:35 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
@@ -52,6 +52,6 @@ resource "aws_route" "DEV_ROUT" {
   
 }
 #   Resource-6: Associate Route Table with Subnet
-resource "aws_route_table_association" "name" {
+resource "aws_route_table_association" "DEV_R_T_" {
   
 }
