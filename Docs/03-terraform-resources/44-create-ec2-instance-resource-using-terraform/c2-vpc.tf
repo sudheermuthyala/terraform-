1c69faa4ebddf73cf355ea14e8228e721d70fbeb
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:36:06 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "DEV_PUB_SG" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
