96fe18448bc8f66279460148e3244b6397f15837
sudheer <sudheerlikeu@gmail.com>
2021-11-27 10:03:46 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c3-ec2-instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "MY_DEV_VM" {
     ami = "ami-0855cab4944392d0a"
     instance_type = "t2.micro"
     key_name = "terraform_k"
-    subnet_id = aws_subnet.DEV_SUBNET
+    subnet_id = aws_subnet.DEV_SUBNET.id
+    
   
 }
