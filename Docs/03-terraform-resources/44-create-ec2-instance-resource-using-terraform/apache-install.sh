57a0c4a6d2ca3bdb8949520e93e42f245cebb219
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:43:19 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/apache-install.sh
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/apache-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo yum update -y
 sudo yum install -y httpd
-sudo service  start  
+sudo systemctl  start  httpd
 sudo systemctl enable httpd
 echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
