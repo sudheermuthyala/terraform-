7e67daff5b3dc6ad9259a312c1d2b0df4aebbd75
sudheer <sudheerlikeu@gmail.com>
2021-11-27 09:34:16 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
+++ Docs/03-terraform-resources/44-create-ec2-instance-resource-using-terraform/c2-vpc.tf
@@ -57,6 +57,7 @@ resource "aws_route_table_association" "DEV_R_T_ASSOCIATION" {
   subnet_id = aws_subnet.DEV_SUBNET.id
   
 }
+
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
