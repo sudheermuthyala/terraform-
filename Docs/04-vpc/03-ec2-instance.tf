646c925e505d8676b6c1b76325e91ea8c82ce321
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:32:01 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/03-ec2-instance.tf
+++ Docs/04-vpc/03-ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "MY_ec2_vM" {
     ami = "ami-0d718c3d715cec4a7"
     key_name = "terraform"
     instance_type = "t2.micro"
-    subnet_id = aws_subnet.DEV-SUBNET-1.i
+    subnet_id = aws_subnet.DEV-SUBNET-1.id
     vpc_security_group_ids = 
   
 }
