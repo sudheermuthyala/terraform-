27ae13a9031ca95a60d0c731a901e1527d5b5c03
sudheer <sudheerlikeu@gmail.com>
2021-11-28 10:09:06 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c1-version.tf
+++ Docs/03-terraform-resources/43-creat-VPC-resource-using-terraform/c1-version.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~> 1.0.11"
   required_providers {
     aws = {
-        version = "~> 3.0.0"
+        version = "~> 3"
         source = "hashicorp/aws"
     }
   }
