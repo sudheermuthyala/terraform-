5da86b1a26628a9dffcfac07e7470d6d85a778fc
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:11:21 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -51,5 +51,7 @@ resource "aws_internet_gateway" "DEV_IGW" {
 resource "aws_route_table" "DEV_ROUT_TABLE" {
     vpc_id = aws_vpc.MY_DEV_VPC.id
 
-    
+    tags = {
+      "Name" = "value"
+    }
 }
