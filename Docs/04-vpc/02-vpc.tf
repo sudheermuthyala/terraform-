e9cfa1334ac21a08045809fae25817be6d18eb67
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:24:21 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "DEV_allo_public" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
