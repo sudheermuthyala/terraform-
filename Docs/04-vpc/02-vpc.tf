d69b048f7f31c47f213c56010f1a2d660de95ced
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:24:41 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -87,9 +87,9 @@ resource "aws_security_group" "DEV_allo_public" {
   }
 
   ingress {
-    description      = "Allow ssh "
-    from_port        = 22
-    to_port          = 22
+    description      = "Allow http "
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
 
