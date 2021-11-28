24c030037b2ecde22ec8564e9bf53c8e3a2971fb
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:12:20 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -57,6 +57,6 @@ resource "aws_route_table" "DEV_ROUT_TABLE" {
 }
 
 # step-5  Create Route in Route Table for Internet Access
-resource "aws_rout" "name" {
+resource "aws_route" "DEV_ROUT" {
   
 }
