9125c1d2580d8bfa113630c34689db0900d1e434
sudheer <sudheerlikeu@gmail.com>
2021-11-28 10:58:20 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -8,4 +8,4 @@ Create Route Table
 Create Route in Route Table for Internet Access
 Associate Route Table with Subnet
 Create Security Group in the VPC with port 80, 22 as inbound open
-# Resource Block
+*/
