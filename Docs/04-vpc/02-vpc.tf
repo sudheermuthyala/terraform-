4493bb9236180c0f4ff8775b37720ac11c7b1bc7
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:22:01 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -74,9 +74,9 @@ resource "aws_route_table_association" "DEV_ROUT_TABLE_ASSO" {
 #step-7  Create Security Group in the VPC with port 22 as inbound open
 
 resource "aws_security_group" "DEV_allo_public" {
-  name        = "allow_tls"
+  name        = "DEV_allo_public"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = 
 
   ingress {
     description      = "TLS from VPC"
