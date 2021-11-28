70d241a4d44eb6d8a0c6081fa764f2db63b844b0
sudheer <sudheerlikeu@gmail.com>
2021-11-28 11:00:51 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -2,7 +2,7 @@
 Create VPC using manual
 
 step1-  Create VPC Resources listed below
-step1-Create VPC
+step2-  Create VPC
 step1-Create Subnet
 step1-Create Internet Gateway
 step1-Create Route Table
