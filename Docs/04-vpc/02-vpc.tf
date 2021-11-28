4388d7b3a4429aa38dd6ffef59ae141512a0eb56
sudheer <sudheerlikeu@gmail.com>
2021-11-28 11:13:15 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -6,7 +6,7 @@ step1-  Create VPC
 step2-  Create Subnet
 step3-  Create Internet Gateway (Attach to required vpc EX: my vpc)
 step4-  Create Route Table
-step1-Create Route in Route Table for Internet Access
+step5-  Create Route in Route Table for Internet Access
 step1-Associate Route Table with Subnet
 step1-Create Security Group in the VPC with port 80, 22 as inbound open
 */
