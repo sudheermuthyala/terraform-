8a8e2e680c0fd3dac03a97ec776a0cb79e4a11f2
sudheer <sudheerlikeu@gmail.com>
2021-11-28 10:58:00 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-vpc/02-vpc.tf
@@ -0,0 +1,10 @@
+Step-04-02: Create VPC using Terraform
+Create VPC Resources listed below
+Create VPC
+Create Subnet
+Create Internet Gateway
+Create Route Table
+Create Route in Route Table for Internet Access
+Associate Route Table with Subnet
+Create Security Group in the VPC with port 80, 22 as inbound open
+# Resource Block
