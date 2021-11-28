60059879226310cc7de3a16b3b3b226d5b1c2f5f
sudheer <sudheerlikeu@gmail.com>
2021-11-28 10:58:50 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/02-vpc.tf
+++ Docs/04-vpc/02-vpc.tf
@@ -2,11 +2,11 @@
 Create VPC using 
 
 stepCreate VPC Resources listed below
-Create VPC
-Create Subnet
-Create Internet Gateway
-Create Route Table
-Create Route in Route Table for Internet Access
-Associate Route Table with Subnet
-Create Security Group in the VPC with port 80, 22 as inbound open
+stepCreate VPC
+stepCreate Subnet
+stepCreate Internet Gateway
+stepCreate Route Table
+stepCreate Route in Route Table for Internet Access
+stepAssociate Route Table with Subnet
+stepCreate Security Group in the VPC with port 80, 22 as inbound open
 */
