0f2db185f624f69bca54bb0ffdeb24906a8c1594
sudheer <sudheerlikeu@gmail.com>
2021-11-28 12:33:41 +0530

    --- Auto-Git Commit ---

--- Docs/04-vpc/03-ec2-instance.tf
+++ Docs/04-vpc/03-ec2-instance.tf
@@ -6,5 +6,16 @@ resource "aws_instance" "MY_ec2_vM" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.DEV-SUBNET-1.id
     vpc_security_group_ids = [aws_security_group.DEV_allo_public.id]
-    
+    user_data = <<-EOF
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl  start  httpd
+    sudo systemctl enable httpd
+    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF
+
+    tags = {
+      "Name" = "webserver"
+    }
 }
