b1faf1df80cf4486a834ad953aa689a0b1500765
sudheer <sudheerlikeu@gmail.com>
2021-11-29 15:23:39 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/50-Usecase-for_each_Maps/terraform-manifiest/c2-s3.tf
+++ Docs/03-terraform-resources/50-Usecase-for_each_Maps/terraform-manifiest/c2-s3.tf
@@ -3,6 +3,11 @@ resource "aws_s3_bucket" "b" {
 
     for_each = {
         dev = "dev-my-tf-test-bucket"
+        dev = "dev-my-tf-test-bucket"
+        dev = "dev-my-tf-test-bucket"
+        dev = "dev-my-tf-test-bucket"
+        dev = "dev-my-tf-test-bucket"
+
     }
   bucket = "my-tf-test-bucket"
   acl    = "private"
