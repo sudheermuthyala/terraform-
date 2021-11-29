d6857a760ebae8745417636c37f9c06650d38575
sudheer <sudheerlikeu@gmail.com>
2021-11-29 15:59:59 +0530

    --- Auto-Git Commit ---

---
+++ Docs/03-terraform-resources/50-Usecase-for_each_Maps/02-for_each_toset_String/iam-users-creation/c1-versions.tf
@@ -0,0 +1,19 @@
+#terraform Block 
+terraform {
+  required_version = "~> 1.0.11"
+  required_providers {
+    aws = {
+      version = "~> 3.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+#Provider Block
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+
+}
+
