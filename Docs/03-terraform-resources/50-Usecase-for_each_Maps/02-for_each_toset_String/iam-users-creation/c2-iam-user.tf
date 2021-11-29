5723a0578c85652e6eb72166c81159485a34db44
sudheer <sudheerlikeu@gmail.com>
2021-11-29 16:17:20 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/50-Usecase-for_each_Maps/02-for_each_toset_String/iam-users-creation/c2-iam-user.tf
+++ Docs/03-terraform-resources/50-Usecase-for_each_Maps/02-for_each_toset_String/iam-users-creation/c2-iam-user.tf
@@ -1,11 +1,12 @@
 resource "aws_iam_user" "MyUser" {
     for_each = toset(["sudheer","sridheer","alaikhya","hakeesh"])
     name  = each.value
-    #name
+    #name = each.key
 }
 
 
 /*
+
 for_each set of strings 
 each.key=each.value
 
