b3495c8fc777fd95728e83debf7c914a2c9357f4
sudheer <sudheerlikeu@gmail.com>
2021-11-30 16:19:29 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MY_ec2_VM" {
     ami = var.ec2_ami_id
     instance_type = "t2.micro"
     key_name = "terraform-k"
-    count = 1
+    count = var.ec2_count
     user_data = <<EOF
         echo "heloworld"
     EOF
