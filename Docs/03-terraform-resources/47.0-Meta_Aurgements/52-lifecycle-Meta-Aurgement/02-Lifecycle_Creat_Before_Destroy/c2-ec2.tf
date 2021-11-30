ef60bb10bc9c1900b99f825c0ae8b3a43efd6af5
sudheer <sudheerlikeu@gmail.com>
2021-11-30 12:40:58 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/02-Lifecycle_Creat_Before_Destroy/c2-ec2.tf
+++ Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/02-Lifecycle_Creat_Before_Destroy/c2-ec2.tf
@@ -13,11 +13,12 @@ resource "aws_instance" "DEMO_ON_WITHOUT_META_AURGEMENT_LIFECYCLE_" {
       "Name" = "web"
     }
 
-    lifecycle {     
-      create_before_destroy = true 
-      /* this is a meta aurgement with a lifecycle of create_before_destroy 
+/* this is a meta aurgement with a lifecycle of create_before_destroy 
         this will do the. action create the resource befor it destroy 
       */
+    lifecycle {     
+      create_before_destroy = true 
+      
     }
-    
+
 }
