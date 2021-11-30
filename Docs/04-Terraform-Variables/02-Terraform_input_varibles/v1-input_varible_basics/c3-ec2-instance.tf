0d08651ce9e57e9b077493a7b80cac81e8d8e945
sudheer <sudheerlikeu@gmail.com>
2021-11-30 16:20:27 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
@@ -4,9 +4,17 @@ resource "aws_instance" "MY_ec2_VM" {
     instance_type = "t2.micro"
     key_name = "terraform-k"
     count = var.ec2_count
-    user_data = <<EOF
-        
+    user_data = <<-EOF
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl  start  httpd
+    sudo systemctl enable httpd
+    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
-  
+
+    tags = {
+      "Name" = "webserver"
+    }
   
 }
