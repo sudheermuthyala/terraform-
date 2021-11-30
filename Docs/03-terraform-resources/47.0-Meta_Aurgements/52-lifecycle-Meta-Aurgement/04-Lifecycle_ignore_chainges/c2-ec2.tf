674e6cf2a4522338877e99582bc9661f64d3e728
sudheer <sudheerlikeu@gmail.com>
2021-11-30 13:12:17 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/04-Lifecycle_ignore_chainges/c2-ec2.tf
+++ Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/04-Lifecycle_ignore_chainges/c2-ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "DEMO_ON_META_AURGEMENT_LIFECYCLE_ignore_chainges" {
 
 
     tags = {
-      "Name" = "web-1"
+      "Name" = ""
     }
 
       /* this is a meta aurgement with a lifecycle of  prevent_destroy
