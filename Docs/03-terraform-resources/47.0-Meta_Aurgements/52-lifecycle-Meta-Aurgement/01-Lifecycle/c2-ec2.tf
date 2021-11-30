7364c0e7b358ee431fe017e029822fffae138688
sudheer <sudheerlikeu@gmail.com>
2021-11-30 12:18:31 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/01-Lifecycle/c2-ec2.tf
+++ Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/01-Lifecycle/c2-ec2.tf
@@ -2,10 +2,10 @@
 
 resource "aws_instance" "DEMO_ON_WITHOUT_META_AURGEMENT_LIFECYCLE_" {
       
-      count = 
+      count = 2
       ami               = "ami-04902260ca3d33422"
       availability_zone = "us-east-1a"
-      #availability_zone = "us-east-1a"
+      availability_zone = "us-east-1a"
       instance_type     = "t2.micro"
 
 }
