b33fc1f02e0d43617648ee7e4c0af12272459af1
sudheer <sudheerlikeu@gmail.com>
2021-11-30 12:55:42 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/03-Lifecycle_Prevent_Destroy/c2-ec2.tf
+++ Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/03-Lifecycle_Prevent_Destroy/c2-ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "DEMO_ON_META_AURGEMENT_LIFECYCLE_prevent_destroy" {
 
     lifecycle {     
       prevent_destroy = true  #Default is false
-      prevent_destroy = true  #Default is false
+      #prevent_destroy = false  #Default is false
     }
 
 }
