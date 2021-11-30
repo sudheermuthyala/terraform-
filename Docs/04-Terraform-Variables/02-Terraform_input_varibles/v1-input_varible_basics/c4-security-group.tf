dda106e980d64422b2026f36a657e88c8d835147
sudheer <sudheerlikeu@gmail.com>
2021-11-30 17:07:46 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c4-security-group.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c4-security-group.tf
@@ -11,13 +11,6 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port        = 0
