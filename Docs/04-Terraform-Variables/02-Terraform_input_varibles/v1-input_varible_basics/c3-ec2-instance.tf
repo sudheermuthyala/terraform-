17ab3c36dc01f21ea9266bba5ac2e0c59e04c16f
sudheer <sudheerlikeu@gmail.com>
2021-11-30 17:11:52 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "MY_ec2_VM" {
     instance_type = "t2.micro"
     key_name = "terraform-k"
     count = var.ec2_count
-    vpc_security_group_ids = [ aws_security_group.allow_ssh, aws_security_group. ]
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.i, aws_security_group.vpc-web ]
     #user_data = file("apache-install.sh")
     user_data = <<-EOF
     #! /bin/bash
