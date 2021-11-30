f0e11e74e59ed137cb083671bf35eb019f577d7f
sudheer <sudheerlikeu@gmail.com>
2021-11-30 16:18:11 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c3-ec2-instance.tf
@@ -1,6 +1,6 @@
 # terraform aws ec2_instance Resource
 resource "aws_instance" "MY_ec2_VM" {
-    ami = "ami-04902260ca3d33422"
+    ami = "
     instance_type = "t2.micro"
     key_name = "terraform-k"
     count = 1
