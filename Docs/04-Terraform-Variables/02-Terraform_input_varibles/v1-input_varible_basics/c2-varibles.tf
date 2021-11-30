effe29d4eef1cc16f5a6b2fe45e850817d1e43ca
sudheer <sudheerlikeu@gmail.com>
2021-11-30 16:16:09 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c2-varibles.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c2-varibles.tf
@@ -1,14 +1,14 @@
+# count
 variable "aws_region" {
   description = "Region in which AWS resource to be created"
   type = string
   default = "us-east-1"
 }
 
+# amiid
 variable "ec2_ami_id" {
   description = "AMI_ID"
   type = string
   default = "ami-04902260ca3d33422"
 }
 
-# amiid
-# count
