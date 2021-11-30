84591512fa1fb1e712c3431c2d82af6db0952488
sudheer <sudheerlikeu@gmail.com>
2021-11-30 17:05:40 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c4-security-group.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v1-input_varible_basics/c4-security-group.tf
@@ -2,7 +2,6 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
@@ -10,7 +9,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
