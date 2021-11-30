36402d035756ac01e33f2508a1910920b54781b4
sudheer <sudheerlikeu@gmail.com>
2021-11-30 12:48:10 +0530

    --- Auto-Git Commit ---

--- Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/03-Lifecycle_Prevent_Destroy/c2-ec2.tf
+++ Docs/03-terraform-resources/47.0-Meta_Aurgements/52-lifecycle-Meta-Aurgement/03-Lifecycle_Prevent_Destroy/c2-ec2.tf
@@ -16,10 +16,9 @@ resource "aws_instance" "DEMO_ON_WITHOUT_META_AURGEMENT_LIFECYCLE_" {
       /* this is a meta aurgement with a lifecycle of create_before_destroy 
         this will do the. action create the resource befor it destroy 
       */
+      
     lifecycle {     
-      
       prevent_destroy = true  #Default is false
-      
     }
 
 }
