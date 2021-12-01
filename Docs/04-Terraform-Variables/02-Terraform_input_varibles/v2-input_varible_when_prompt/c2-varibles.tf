dbf16934d2394bcfcd62f6cba30f49b2007f8a39
sudheer <sudheerlikeu@gmail.com>
2021-12-01 09:41:20 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/v2-input_varible_when_prompt/c2-varibles.tf
@@ -0,0 +1,27 @@
+
+#region
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
+
+# amiid
+variable "ec2_ami_id" {
+  description = "AMI_ID"
+  type = string
+  default = "ami-04902260ca3d33422"
+}
+
+# count
+variable "ec2_count" {
+    description = "Ec2_innstance count"
+    type = number
+    default = 1
+  
+}
+
+
+/*
+prompt varibles we pass the varibles from CLI when it prompt
+*/
