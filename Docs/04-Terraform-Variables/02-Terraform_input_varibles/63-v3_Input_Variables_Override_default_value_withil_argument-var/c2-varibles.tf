47c77ece8001e592f2cebfc87a6adf4b27988b0f
sudheer <sudheerlikeu@gmail.com>
2021-12-01 10:10:55 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/63-v3_Input_Variables_Override_default_value_withil_argument-var/c2-varibles.tf
@@ -0,0 +1,33 @@
+# input Varibles
+#region
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
+
+# amiid
+variable "ec2_ami_id" {
+  description = "AMI_ID"
+  type = string
+  default = "ami-04902260ca3d33422"
+}
+
+# count
+variable "ec2_count" {
+    description = "Ec2_innstance count"
+    type = number
+    default = 1
+  
+}
+
+
+/*
+prompt varibles we will pass the varibles from CLI when it prompt
+*/
+
+
+variable "ec2_instance_type" {
+  description = "instance Type"
+  type = string
+}
