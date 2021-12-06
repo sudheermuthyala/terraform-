bc60785295f7ecdcf76e5a262b929a92729f3e51
sudheer <sudheerlikeu@gmail.com>
2021-12-06 18:44:58 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c2-varibles.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c2-varibles.tf
@@ -22,15 +22,6 @@ variable "ec2_count" {
   
 }
 
-variable "ec2_instance_tags" {
-  description = "EC2 instance tags"
-  type = map(string)
-  default = {
-    "Name" = "web"
-    "project" = "roboshop"
-    "env" = "dev"
-  }
-}
 variable "ec2_instance_type" {
   description = "instance Type"
   type = list(string)
