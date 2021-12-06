10de613089042664c51b479b7d07b35eed8e364c
sudheer <sudheerlikeu@gmail.com>
2021-12-06 18:58:01 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c3-ec2-instance.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "MY_ec2_VM" {
     ami = var.ec2_ami_id
     #instance_type = var.ec2_instance_type[0]
     instance_type = var.ec2_instance_type["small_app"]
+    instance_type = var.ec2_instance_type["small_app"]
+    instance_type = var.ec2_instance_type["small_app"]
     key_name = "terraform-k"
     count = var.ec2_count
     vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
