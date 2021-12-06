7b54e09a50976ac125d40a913aecb0a99da72a77
sudheer <sudheerlikeu@gmail.com>
2021-12-06 18:52:50 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c2-varibles.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/68-v8-Implement-complex-type-cosntructors-like-list-and-maps/02-map/c2-varibles.tf
@@ -36,7 +36,7 @@ variable "ec2_instance_type" {
   description = "EC2-instance types"
   type = map(string)
   default = {
-    "small_web" = "t2.small"
-    mediun
+    "small_" = "t2.small"
+    medium_web
   }
 }
