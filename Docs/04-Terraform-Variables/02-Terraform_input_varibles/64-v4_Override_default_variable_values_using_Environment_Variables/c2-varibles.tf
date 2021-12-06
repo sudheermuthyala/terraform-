7fd40f54605301af03d46a5f75dc63c5a50019b6
sudheer <sudheerlikeu@gmail.com>
2021-12-06 14:55:15 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/64-v4_Override_default_variable_values_using_Environment_Variables/c2-varibles.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/64-v4_Override_default_variable_values_using_Environment_Variables/c2-varibles.tf
@@ -35,3 +35,7 @@ variable "ec2_instance_type" {
 export  TF_VAR_ec2_count=2
 export  TF_VAR_ec2_instance_type=t2.small
 */
+/*
+export  TF_VAR_ec2_count=2
+export  TF_VAR_ec2_instance_type=t2.small
+*/
