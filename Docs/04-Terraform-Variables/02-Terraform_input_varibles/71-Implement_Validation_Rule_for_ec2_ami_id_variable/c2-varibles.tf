9b9d6ff4961becb106fb518899a2b29521cdbcb6
sudheer <sudheerlikeu@gmail.com>
2021-12-06 21:43:53 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/02-Terraform_input_varibles/71-Implement_Validation_Rule_for_ec2_ami_id_variable/c2-varibles.tf
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/71-Implement_Validation_Rule_for_ec2_ami_id_variable/c2-varibles.tf
@@ -12,6 +12,7 @@ variable "ec2_ami_id" {
   type = string
   default = "ami-04902260ca3d33422"
   validation {
+    #condition = length(var.ec2_ami_id) > 4 && substr(var.ec2_ami_id, 0,4) == "ami-"
     condition = length(var.ec2_ami_id) > 4 && substr(var.ec2_ami_id, 0,4) == "Kmi-"
     error_message = "The ec2_ami_id value must be a valid AMI id, starting with \"ami-\"."
   }
