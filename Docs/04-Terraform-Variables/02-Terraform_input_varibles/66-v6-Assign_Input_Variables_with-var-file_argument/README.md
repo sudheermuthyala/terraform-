b0ae432f6e2b499e69c0f9a278bf8ab6319857ce
sudheer <sudheerlikeu@gmail.com>
2021-12-06 15:21:40 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/66-v6-Assign_Input_Variables_with-var-file_argument/README.md
@@ -0,0 +1,24 @@
+# 05: Assign Input Variables from terraform.tfvars 
+
+- v5-Input-Variables-Assign-with-terraform-tfvars
+- Create a file named terraform.tfvars and define variables
+- If the file name is terraform.tfvars, terraform will auto-load the variables present in this file by overriding the default values in variables.tf
+
+# Initialize Terraform
+-   terraform init
+
+# Validate Terraform configuration files
+-   terraform validate
+
+# Format Terraform configuration files
+-   terraform fmt
+
+# Review the terraform plan
+-   terraform plan
+
+# Create Resources
+-   terraform apply
+
+# Access Application
+-   http://<Elastic-IP-Address>
+
---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/66-v6-Assign_Input_Variables_with-var-file_argument/c2-varibles.tf
@@ -0,0 +1,34 @@
+# input Varibles
+#region
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
+
+# amiid
+variable "ec2_ami_id" {
+  description = "AMI_ID"
+  type = string
+  default = "ami-04902260ca3d33422"
+}
+
+#over right the default varibles  using -var from cli 
+# count
+variable "ec2_count" {
+    description = "Ec2_innstance count"
+    type = number
+    default = 1
+  
+}
+variable "ec2_instance_type" {
+  description = "instance Type"
+  type = string
+  default = "t3.small"
+}
+
+/*
+terraform.tfvars
+  ec2_count           =  3
+  ec2_instance_type   =  "t2.small"
+*/
