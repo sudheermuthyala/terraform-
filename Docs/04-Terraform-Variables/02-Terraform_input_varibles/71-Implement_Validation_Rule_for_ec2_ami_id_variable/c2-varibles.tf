747ea93581949ca0427a02ee3ab4afed440bd413
sudheer <sudheerlikeu@gmail.com>
2021-12-06 21:28:11 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/71-Implement_Validation_Rule_for_ec2_ami_id_variable/c2-varibles.tf
@@ -0,0 +1,30 @@
+# input Varibles
+#region
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
+
+# amiid
+variable "ec2_ami_id" {
+  description = "AMI_ID"
+  type = string
+  default = "ami-04902260ca3d33422"
+}
+
+#over right the default varibles  using -var from cli 
+# count
+variable "ec2_count" {
+    description = "Ec2_innstance count"
+    type = number
+    default = 1
+  
+}
+variable "ec2_instance_type" {
+  description = "instance Type"
+  type = list(string)
+  default = [ "t2.small" , "t3.large" , "t3.xlarge" ]
+}
+
+
---
+++ Docs/04-Terraform-Variables/02-Terraform_input_varibles/71-Implement_Validation_Rule_for_ec2_ami_id_variable/c3-ec2-instance.tf
@@ -0,0 +1,24 @@
+# Resource aws ec2_instance terraform
+resource "aws_instance" "MY_ec2_VM" {
+    ami = var.ec2_ami_id
+    #instance_type = var.ec2_instance_type[0]
+    #instance_type = var.ec2_instance_type[1]
+    instance_type = var.ec2_instance_type[2]
+    key_name = "terraform-k"
+    count = var.ec2_count
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
+    #user_data = file("apache-install.sh")
+    user_data = <<-EOF
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl  start  httpd
+    sudo systemctl enable httpd
+    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF
+
+    tags = {
+      "Name" = "webserver"
+    }
+  
+}
