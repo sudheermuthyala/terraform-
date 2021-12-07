c2de600d443308b9953f49c718014aec1a97a356
sudheer <sudheerlikeu@gmail.com>
2021-12-07 14:57:19 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/04-local-values/79-local-value-implementation/c3-s3-bucket.tf
+++ Docs/04-Terraform-Variables/04-local-values/79-local-value-implementation/c3-s3-bucket.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "mys3" {
-  bucket = "${var.}-${}-buscket"
+  bucket = "${var.app_name}-${}-buscket"
   acl = "private"
   tags = {
     "Name" = "value"
     "env"  = var.envinorment_name
   }
 }
+
+variable "app_name" {
