733fd40a36a14d4ea629c1e93554cca67b32028f
sudheer <sudheerlikeu@gmail.com>
2021-12-07 11:09:27 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c3-ec2-instance.tf
@@ -0,0 +1,21 @@
+# resource: 8 aws_ec2_instance 
+resource "aws_instance" "MY_DEV_VM" {
+    ami = "ami-04902260ca3d33422"
+    instance_type = "t2.micro"
+    key_name = "terraform-k"
+    subnet_id = aws_subnet.DEV_SUBNET.id
+    vpc_security_group_ids = [ aws_security_group.DEV_PUB_SG.id ]
+    user_data = file("apache-install.sh")    ## file(function) used 
+    # user_data = <<-EOF
+    # #! /bin/bash
+    # sudo yum update -y
+    # sudo yum install -y httpd
+    # sudo systemctl  start  httpd
+    # sudo systemctl enable httpd
+    # echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    # EOF
+
+    tags = {
+      "Name" = "webserver"
+    }
+}
