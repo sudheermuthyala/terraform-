97886c7f3a59522f54bcb47694ffd80c7800b2e5
sudheer <sudheerlikeu@gmail.com>
2021-12-07 16:25:39 +0530

    --- Auto-Git Commit ---

--- Docs/05-terraform-detasources/02-datasources/c3-ec2-instance.tf
+++ Docs/05-terraform-detasources/02-datasources/c3-ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MY_ec2_VM" {
     ami = var.ec2_ami_id
     instance_type = var.ec2_instance_type 
     key_name = "terraform-k"
-    count = var.ec2_count
+    #count = var.ec2_count
     vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
     #user_data = file("apache-install.sh")
     user_data = <<-EOF
