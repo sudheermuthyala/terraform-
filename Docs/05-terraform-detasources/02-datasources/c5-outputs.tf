cafaaf021a42d11ebd0567a4c71086bdacb23fe1
sudheer <sudheerlikeu@gmail.com>
2021-12-07 16:38:53 +0530

    --- Auto-Git Commit ---

--- Docs/05-terraform-detasources/02-datasources/c5-outputs.tf
+++ Docs/05-terraform-detasources/02-datasources/c5-outputs.tf
@@ -1,8 +1,8 @@
 output "ec2_instancnce_public-ip" {
-  description = "ec2 instance ip"
+  description = "ec2 instance_ip"
   value = aws_instance.MY_ec2_VM.id
 }
 
 output "ec2_instance_private_ip" {
-  description = "ec2 instance_private"
+  description = "ec2_instance_private"
 }
