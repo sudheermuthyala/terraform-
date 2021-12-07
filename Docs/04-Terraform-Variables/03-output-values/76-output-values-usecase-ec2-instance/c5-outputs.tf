a7c06b6831f436fac3c93091b755597a148008bf
sudheer <sudheerlikeu@gmail.com>
2021-12-07 11:35:51 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c5-outputs.tf
+++ Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c5-outputs.tf
@@ -3,9 +3,9 @@
 #Attriute Reference: EC2 instance public IP
 output "ec2_instance_publicip" {
   description   = "EC2 instance Publice ip"
-  value         = ec2_instance.MY_ec2_VM.public_ip
+  value         = aws_instance.MY_ec2_VM.public_ip
 }
-
+aws_instance
 #Aurgement Reference: EC2 instance private IP
 output "ec2_instance_privateip" {
   description = "Ec2 instance privete ip"
