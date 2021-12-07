fd3ef27fe676387112d4aca6cf87dacf2cd28c79
sudheer <sudheerlikeu@gmail.com>
2021-12-07 11:41:00 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c3-ec2-instance.tf
+++ Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c3-ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MY_ec2_VM" {
     ami = var.ec2_ami_id
     instance_type = "t2.micro"
     key_name = "terraform-k"
-    #count = var.ec2_count ## Hear we are not added cout 
+    #count = var.ec2_count                ## Hear we are not added cout 
     vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
     #user_data = file("apache-install.sh")
     user_data = <<-EOF
