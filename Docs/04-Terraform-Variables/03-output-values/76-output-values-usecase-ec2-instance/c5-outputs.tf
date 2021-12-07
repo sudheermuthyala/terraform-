7307fa6f60b455863726f49e5ccb49fa48ef8c78
sudheer <sudheerlikeu@gmail.com>
2021-12-07 11:19:35 +0530

    --- Auto-Git Commit ---

--- Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c5-outputs.tf
+++ Docs/04-Terraform-Variables/03-output-values/76-output-values-usecase-ec2-instance/c5-outputs.tf
@@ -1,6 +1,6 @@
 #Define Output Values
-#Attriute Reference: EC2 instance public IP
 
+#Attriute Reference: EC2 instance public IP
 output "ec2_instance_publicip" {
   description   = "EC2 instance Publice ip"
   value         = ec2_instance.MY_ec2_VM.public_ip
