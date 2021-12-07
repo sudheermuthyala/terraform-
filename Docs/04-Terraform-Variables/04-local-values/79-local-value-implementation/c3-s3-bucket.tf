56d11e39777fe2b34a09f87e58e5c677743922c2
sudheer <sudheerlikeu@gmail.com>
2021-12-07 14:33:22 +0530

    --- Auto-Git Commit ---

---
+++ Docs/04-Terraform-Variables/04-local-values/79-local-value-implementation/c3-s3-bucket.tf
@@ -0,0 +1,22 @@
+# Resource aws ec2_instance terraform
+resource "aws_instance" "MY_ec2_VM" {
+    ami = var.ec2_ami_id
+    instance_type = "t2.micro"
+    key_name = "terraform-k"
+    #count = var.ec2_count                ## Hear we are not added cout 
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
+    #user_data = file("apache-install.sh")
+    user_data = <<-EOF
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl  start  httpd
+    sudo systemctl enable httpd
+    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF
+
+    tags = {
+      "Name" = "webserver"
+    }
+  
+}
