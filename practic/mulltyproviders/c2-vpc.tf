adf0873b009b380bd5e3debaf0872ae5730f3281
sudheer <sudheerlikeu@gmail.com>
2021-12-10 17:31:22 +0530

    --- Auto-Git Commit ---

--- practic/mulltyproviders/c2-vpc.tf
+++
@@ -1,7 +0,0 @@
-# vpc resource
-resource "aws_vpc" "lab-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    "Name" = "labvpc"
-  }
-}
--- practic/mulltyproviders/c4-vpc-ap-south.tf
+++
@@ -1,8 +0,0 @@
-# vpc resource
-resource "aws_vpc" "lab-vpc" {
-  cidr_block = "10.0.0.0/16"
-  provider = aws.aws.ap-south-1
-  tags = {
-    "Name" = "labvpc"
-  }
-}
