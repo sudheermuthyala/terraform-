bdcef56ce6b83d68991e8998cdbf0440e0ad7965
sudheer <sudheerlikeu@gmail.com>
2021-12-10 14:42:20 +0530

    --- Auto-Git Commit ---

--- practic/mulltyproviders/c1-version.tf
+++ practic/mulltyproviders/c1-version.tf
@@ -1,10 +1,15 @@
 terraform {
   required_version = "~> 1.11.1"
   required_providers {
-    aws  {
+    aws = {
         source = "hashicorp/aws"
         version = "~> 3.0"
 
     }
   }
+}
+
+
+provider "" {
+  
 }
