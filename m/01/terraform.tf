28788a4b2418bb6a91ecf3cd27dd852e81b290ff
sudheer <sudheerlikeu@gmail.com>
2021-12-12 10:37:48 +0530

    --- Auto-Git Commit ---

--- m/01/terraform.tf
+++ m/01/terraform.tf
@@ -19,8 +19,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "myVM" {
-   ami           = "ami-0ed9277fb7eb570c9"
-  #ami           = "ami-052cef05d01020f1d"
+  # ami           = "ami-0ed9277fb7eb570c9"
+  ami           = "ami-052cef05d01020f1d"
   key_name      = "murali"
   instance_type = "t2.micro"
   #availability_zone = "us-east-1b"
