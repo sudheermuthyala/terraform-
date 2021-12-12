c66c71316cf70f46d271cde571abc5abc1398b59
sudheer <sudheerlikeu@gmail.com>
2021-12-12 14:57:01 +0530

    --- Auto-Git Commit ---

--- 05-terraform-detasources/02-datasources/c6-data.tf
+++ 05-terraform-detasources/02-datasources/c6-data.tf
@@ -1,7 +1,8 @@
 data "aws_ami" "amzlinux" {
-  #most_recent = true
+  most_recent = true
   
-  #most_recent = true 
+  #most_recent = true //│ Error: Your query returned more than one result. Please try a more specific search criteria, or set `most_recent` attribute to true.
+
   owners = [ "amazon" ]
   filter {
     name = "name"
