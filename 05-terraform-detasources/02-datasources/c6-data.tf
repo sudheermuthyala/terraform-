7ae508093c54cc3ed3efb349ff84d00731f30b3b
sudheer <sudheerlikeu@gmail.com>
2021-12-12 14:36:42 +0530

    --- Auto-Git Commit ---

--- 05-terraform-detasources/02-datasources/c6-data.tf
+++ 05-terraform-detasources/02-datasources/c6-data.tf
@@ -1,27 +1,27 @@
-data "aws_ami" "amzlinux" {  
-  most_recent = true
-  owners = [ "amazon" ]
+# data "aws_ami" "amzlinux" {  
+#   most_recent = true
+#   owners = [ "amazon" ]
 
-  filter {
-      name = "name"
-      values = [ "amzn2-ami-hvm-*" ]
-  }
+#   filter {
+#       name = "name"
+#       values = [ "amzn2-ami-hvm-*" ]
+#   }
 
-  filter {
-    name = "root-device-type"
-    values = [ "ebs" ]
-  }
+#   filter {
+#     name = "root-device-type"
+#     values = [ "ebs" ]
+#   }
 
-  filter {
-    name = "virtualization-type"
-    values = [ "hvm" ]
-  }
+#   filter {
+#     name = "virtualization-type"
+#     values = [ "hvm" ]
+#   }
   
-  filter {
-    name = "Architecture"
-    values = [ "x86_64" ]
-  }
-}
+#   filter {
+#     name = "Architecture"
+#     values = [ "x86_64" ]
+#   }
+# }
 
 data "aws_ami" "amzlinux" {
   most_recent = true
