779dc4c042962f739e172b15df6faeda4987c53c
sudheer <sudheerlikeu@gmail.com>
2021-12-13 11:04:07 +0530

    --- Auto-Git Commit ---

--- 06-terraform-state/85-S3-backend-configuration/c5-outputs.tf
+++ 06-terraform-state/85-S3-backend-configuration/c5-outputs.tf
@@ -9,6 +9,6 @@ output "ec2_instance_private_ip" {
 }
 
 output "ec2_public_dns" {
-  description = "ec2_instance_private"
+  descriptin  = "public_Dns"
   value = aws_instance.MY_ec2_VM.public_dns
 }
