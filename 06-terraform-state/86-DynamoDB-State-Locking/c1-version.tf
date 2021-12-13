a3f2eaf94bd740764c2d677ae9618b43a13e9dd9
sudheer <sudheerlikeu@gmail.com>
2021-12-13 12:42:53 +0530

    --- Auto-Git Commit ---

--- 06-terraform-state/86-DynamoDB-State-Locking/c1-version.tf
+++ 06-terraform-state/86-DynamoDB-State-Locking/c1-version.tf
@@ -14,7 +14,7 @@ terraform {
     region = "us-east-1"
 
     # For State Locking in Dynamodb_Table
-    dynamodb_table = "terraformdheer_devops"
+    dynamodb_table = "terraform_dheer_devops"
 
   
   }
