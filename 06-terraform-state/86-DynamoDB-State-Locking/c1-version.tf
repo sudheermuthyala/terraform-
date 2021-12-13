69cf0a916235cab0172f7b880f3c0ed1ca478e2d
sudheer <sudheerlikeu@gmail.com>
2021-12-13 12:42:16 +0530

    --- Auto-Git Commit ---

---
+++ 06-terraform-state/86-DynamoDB-State-Locking/c1-version.tf
@@ -0,0 +1,28 @@
+# treeaform Block
+terraform {
+  required_version = "~> 1.0.11"
+  required_providers {
+    aws = {
+        version = "~> 3.0"
+        source = "hashicorp/aws"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage with State Locking
+  backend "s3" {
+    bucket = "terraformsu"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
+  
+  }
+}
+
+#provider block
+
+provider "aws" {
+  profile = "default"
+  region =  "us-east-1"
+}
+
+# For State Locking in Dynamodb_Table
+  dynamodb_table = "terraform_dheer_devops"
