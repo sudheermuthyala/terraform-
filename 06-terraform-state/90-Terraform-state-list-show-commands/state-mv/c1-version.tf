6f428d80163165a68cfd321c2aa46fffb0ba9b7c
sudheer <sudheerlikeu@gmail.com>
2021-12-17 12:41:22 +0530

    --- Auto-Git Commit ---

--- 06-terraform-state/90-Terraform-state-list-show-commands/state-mv/c1-version.tf
+++ 06-terraform-state/90-Terraform-state-list-show-commands/state-mv/c1-version.tf
@@ -7,7 +7,7 @@ terraform {
         source = "hashicorp/aws"
     }
   }
-  # Adding Backend as S3 for Remote State Storage with State Locking
+# Adding Backend as S3 for Remote State Storage with State Locking
 #   backend "s3" {
 #     bucket = "terraformsud"
 #     key    = "qa/terraform.tfstate"
@@ -18,7 +18,7 @@ terraform {
 
   
 #   }
-# }
+}
 
 #provider block
 
