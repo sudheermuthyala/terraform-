3877bb65c35a3b1a491526173b3fae2cacd10d9e
sudheer <sudheerlikeu@gmail.com>
2021-12-20 12:55:41 +0530

    --- Auto-Git Commit ---

--- 06-terraform-state/94-terrafiorm-resource-targeting/c4-security-group.tf
+++ 06-terraform-state/94-terrafiorm-resource-targeting/c4-security-group.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "vpc-web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ingress {
+    description = "Allow Port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Allow Port 443"
@@ -57,5 +64,5 @@ resource "aws_security_group" "vpc-web" {
   }
   tags = {
     Name = "allow_httpd"
-  }
+      }
 }
