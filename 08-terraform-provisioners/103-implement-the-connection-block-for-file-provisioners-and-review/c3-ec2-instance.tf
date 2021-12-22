f77b6c0872c9100bd5a6f788b3ed12f8508df125
sudheer <sudheerlikeu@gmail.com>
2021-12-22 17:42:25 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/103-implement-the-connection-block-for-file-provisioners-and-review/c3-ec2-instance.tf
+++ 08-terraform-provisioners/103-implement-the-connection-block-for-file-provisioners-and-review/c3-ec2-instance.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "MY_ec2_VM" {
   # Copies the app1 folder to /tmp - FOLDER COPY
   provisioner "file" {
     source = "Application/cart-app"
-    destination = 
+    destination = "/tmp/"
   }
 
 }
