04397ed38d46770279d45b90821954a59ac6f7f6
sudheer <sudheerlikeu@gmail.com>
2021-12-22 12:12:59 +0530

    --- Auto-Git Commit ---

---
+++ 07-terraform-workspace/101-/c5-outputs.tf
@@ -0,0 +1,14 @@
+output "ec2_instancnce_public-ip" {
+  description = "ec2_instance_ip"
+  value = aws_instance.MY_ec2_VM.*.public_ip
+}
+
+output "ec2_instance_private_ip" {
+  description = "ec2_instance_private"
+  value = aws_instance.MY_ec2_VM.*.private_ip
+}
+
+output "ec2_public_dns" {
+  description  = "public_Dns"
+  value = aws_instance.MY_ec2_VM.*.public_dns
+}
