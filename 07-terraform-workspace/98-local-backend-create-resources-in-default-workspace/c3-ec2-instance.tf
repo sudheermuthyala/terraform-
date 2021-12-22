46af694ad6316eb60e0d9ce1e68d94476beb3cf0
sudheer <sudheerlikeu@gmail.com>
2021-12-22 10:08:35 +0530

    --- Auto-Git Commit ---

--- 07-terraform-workspace/98-local-backend-create-resources-in-default-workspace/c3-ec2-instance.tf
+++ 07-terraform-workspace/98-local-backend-create-resources-in-default-workspace/c3-ec2-instance.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "MY_ec2_VM" {
     EOF
 
     tags = {
-      "Name" = "webserver"
+      "Name" = ""
       "demotag"  = "refreshtags"
       "target" = "Target-Test-1"   // Adding New Tags for -target 
 
