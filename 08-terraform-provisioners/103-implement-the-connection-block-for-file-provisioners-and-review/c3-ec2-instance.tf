ff1fa8bbc7b13f4c83b2fe991cf0d402060404e0
sudheer <sudheerlikeu@gmail.com>
2021-12-22 17:20:59 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/103-implement-the-connection-block-for-file-provisioners-and-review/c3-ec2-instance.tf
+++ 08-terraform-provisioners/103-implement-the-connection-block-for-file-provisioners-and-review/c3-ec2-instance.tf
@@ -26,6 +26,7 @@ resource "aws_instance" "MY_ec2_VM" {
     type = "ssh"
     host = self.public_ip   # Understand What is a Public IP
     user = "ec2-user"
-    password
+    password = ""
+    
   }
 }
