c0fc929ad6eda29a8e4117339d513b689e78d841
sudheer <sudheerlikeu@gmail.com>
2021-12-23 07:52:00 +0530

    --- Auto-Git Commit ---

---
+++ 08-terraform-provisioners/104-file-provisioner-execute-the-terraform-command/c4-security-group.tf
@@ -0,0 +1,68 @@
+# terraform security groups 
+# Create Security Group - SSH Traffic
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh-${terraform.workspace}"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_httpd"
+  }
+}
+
+# Create Security Group - Web Traffic
+
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web-${terraform.workspace}"
+  description = "Dev VPC Web"
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    description = "Allow Port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_httpd"
+  }
+}
