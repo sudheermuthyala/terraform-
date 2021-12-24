512082f3e235925cee6ff5c8ef5b61b531ec618b
sudheer <sudheerlikeu@gmail.com>
2021-12-24 12:33:01 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
+++ 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
@@ -1,6 +1,6 @@
 # Resource aws ec2_instance terraform
 resource "aws_instance" "MY_ec2_VM" {
-  count                  = terraform.workspace == "default" ? 2 : 1
+  #count                  = terraform.workspace == "default" ? 2 : 1
   ## this is a condition i am writing for to create instance according to the workspace
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = var.ec2_instance_type
