2b59d66e4b5388c26e2e180eb079115d49c10289
sudheer <sudheerlikeu@gmail.com>
2021-12-24 11:27:14 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
+++ 08-terraform-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
@@ -64,13 +64,7 @@ resource "aws_instance" "MY_ec2_VM" {
 
   #   }
 
-## ## Failure Behaviour: Understand Decission making when provisioner fails (Continue/Fail)
-# Test-1: Without on_failure attribute which will fail terraform apply
- # Copies the file-copy.html file to /var/www/html/file-copy.html
-  provisioner "file" {
-    source      = "Applications/application.html"
-    destination = "/var/www/html/application.html"
-   }
+
 
 }
 
