ab01f9e5c97132c2d66eff765b840e1e21eb3737
sudheer <sudheerlikeu@gmail.com>
2021-12-24 10:43:09 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
+++ 08-terraform-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
@@ -1,13 +1,14 @@
 # Resource aws ec2_instance terraform
 resource "aws_instance" "MY_ec2_VM" {
-    count = terraform.workspace == "default" ? 2 : 1 ## this is a condition i am writing for to create instance according to the workspace
-    ami = data.aws_ami.amzlinux.id
-    instance_type = var.ec2_instance_type 
-    key_name = "terraform"
-    #count = var.ec2_count
-    vpc_security_group_ids = [ aws_security_group.allow_ssh.id,  aws_security_group.vpc-web.id ]
-    #user_data = file("apache-install.sh")
-    user_data = <<-EOF
+  count                  = terraform.workspace == "default" ? 2 : 1
+  ## this is a condition i am writing for to create instance according to the workspace
+  ami                    = data.aws_ami.amzlinux.id
+  instance_type          = var.ec2_instance_type
+  key_name               = "terraform"
+  #count = var.ec2_count
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.vpc-web.id]
+  #user_data = file("apache-install.sh")
+  user_data              = <<-EOF
     #! /bin/bash
     sudo yum update -y
     sudo yum install -y httpd
@@ -16,50 +17,50 @@ resource "aws_instance" "MY_ec2_VM" {
     echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
 
-    tags = {
-      "Name" = "vm-${terraform.workspace}-${count.index}"
-      # "demotag"  = "refreshtags"
-      # "target" = "Target-Test-1"   // Adding New Tags for -target 
+  tags = {
+    "Name" = "vm-${terraform.workspace}-${count.index}"
+    # "demotag"  = "refreshtags"
+    # "target" = "Target-Test-1"   // Adding New Tags for -target
 
-    }
+  }
 
   # Connection Block for Provisioners to connect to EC2 Instance
   connection {
-    type = "ssh"
-    host = self.public_ip   # Understand What is a Public IP
-    user = "ec2-user"
-    password = ""
+    type        = "ssh"
+    host        = self.public_ip   # Understand What is a Public IP
+    user        = "ec2-user"
+    password    = ""
     private_key = file("private-key/terraform.pem")
   }
 
-#  # Copies the application.html file to /tmp/application.html
-#   provisioner "file" {
-#     source =  "Applications/application.html"
-#     destination = "/tmp/application.html"
-#   }
-#   # Copies the string in content into "/tmp/application_terraform.log"
-#   provisioner "file" {
-#     content = "ami used: ${self.ami}" # Understand What is "Self"
-#     destination = "/tmp/application_terraform.log"
+  #  # Copies the application.html file to /tmp/application.html
+  #   provisioner "file" {
+  #     source =  "Applications/application.html"
+  #     destination = "/tmp/application.html"
+  #   }
+  #   # Copies the string in content into "/tmp/application_terraform.log"
+  #   provisioner "file" {
+  #     content = "ami used: ${self.ami}" # Understand What is "Self"
+  #     destination = "/tmp/application_terraform.log"
 
-#   }
-#   # Copies the cart-app folder to /tmp - FOLDER COPY
-#   provisioner "file" {
-#     source = "Applications/cart-app"
-#     destination = "/tmp"
-#   }
-  
-#   # Copies all files and folders in "Application/catalogue-app" to /tmp - CONTENTS of FOLDER WILL BE COPIED
+  #   }
+  #   # Copies the cart-app folder to /tmp - FOLDER COPY
+  #   provisioner "file" {
+  #     source = "Applications/cart-app"
+  #     destination = "/tmp"
+  #   }
 
-#   provisioner "file" {
-#     source = "Applications/catalogue-app/"
-#     destination = "/tmp"
-#   }
+  #   # Copies all files and folders in "Application/catalogue-app" to /tmp - CONTENTS of FOLDER WILL BE COPIED
 
-#  # Copies the  Application/cart-app/cart-1.html file to /tmp/cart-1.html 
-#   provisioner "file" {
-#   source = "Applications/cart-app/cart-1.html"
-#   destination = "/tmp/cart-1.html"
+  #   provisioner "file" {
+  #     source = "Applications/catalogue-app/"
+  #     destination = "/tmp"
+  #   }
 
-#   }
+  #  # Copies the  Application/cart-app/cart-1.html file to /tmp/cart-1.html
+  #   provisioner "file" {
+  #   source = "Applications/cart-app/cart-1.html"
+  #   destination = "/tmp/cart-1.html"
+
+  #   }
 }
