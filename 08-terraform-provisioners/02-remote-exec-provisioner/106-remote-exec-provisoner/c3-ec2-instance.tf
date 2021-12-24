b10ba291f130ad61d87e468d475a148d20938019
sudheer <sudheerlikeu@gmail.com>
2021-12-24 12:33:12 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
+++ 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
@@ -7,15 +7,15 @@ resource "aws_instance" "MY_ec2_VM" {
   key_name               = "terraform"
   #count = var.ec2_count
   vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.vpc-web.id]
-  #user_data = file("apache-install.sh")
-  user_data              = <<-EOF
-    #! /bin/bash
-    sudo yum update -y
-    sudo yum install -y httpd
-    sudo systemctl  start  httpd
-    sudo systemctl enable httpd
-    echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
-    EOF
+  user_data = file("apache-install.sh")
+  # user_data              = <<-EOF
+  #   #! /bin/bash
+  #   sudo yum update -y
+  #   sudo yum install -y httpd
+  #   sudo systemctl  start  httpd
+  #   sudo systemctl enable httpd
+  #   echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+  #   EOF
 
   tags = {
     "Name" = "vm-${terraform.workspace}-${count.index}"
