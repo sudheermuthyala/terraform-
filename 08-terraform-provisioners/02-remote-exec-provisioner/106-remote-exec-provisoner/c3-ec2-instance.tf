087a10aa2b9480e1c4231a241fbaf3df9c739c6d
sudheer <sudheerlikeu@gmail.com>
2021-12-24 13:21:09 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
+++ 08-terraform-provisioners/02-remote-exec-provisioner/106-remote-exec-provisoner/c3-ec2-instance.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "MY_ec2_VM" {
   }
 
  provisioner "local-exec" {
-   command = "echo ${aws_instance.MY_ec2_VM.p}"
+   command = "echo ${aws_instance.MY_ec2_VM.public_ip}"
  }
 }
 
