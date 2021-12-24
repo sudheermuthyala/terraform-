a12ab3149b6f99e6fbdc59f01a41b3c0c719d200
sudheer <sudheerlikeu@gmail.com>
2021-12-24 12:23:18 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/01-file-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
+++ 08-terraform-provisioners/01-file-provisioners/105-file-provisioner-failure-behaviour/c3-ec2-instance.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "MY_ec2_VM" {
 
   provisioner "remote-exec" {
     inline = [
-      "sleep 120", # will sleep for 120 secondes 
+      "sleep 120", # will sleep for 120 secondes to ensure 
       "sudo cp /tmp/application.html /var/www/html"
     ]
 
