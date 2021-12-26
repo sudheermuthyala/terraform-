d3c0665d54a0f9566dfff4312f7e20accb6b261d
sudheer <sudheerlikeu@gmail.com>
2021-12-26 15:48:52 +0530

    --- Auto-Git Commit ---

---
+++ 08-terraform-provisioners/04-null-resoure/109-/README.md
@@ -0,0 +1,29 @@
+## Terraform Null Resource
+## Introduction
+- Reference-[Null provider](https://registry.terraform.io/providers/hashicorp/null/latest/docs)
+    - Null Provider is a **rather-unusual provider that has constructs that intentionally do Nothing**
+    - But in terraform in for few specfic usecases ther will be a need for `null_resources`
+    so for that purpose using the `null_resource` you need to add the **Null-Provide**
+
+    - If you add the **null_provider** in `version.tf` then you can start using the **null-resources**
+- Reference-[Null Resource or Provisioners Without a Resource](https://www.terraform.io/language/resources/provisioners/null_resource)
+    - If you need to run provisioners that aren't directly associated with a specific resource, you can associate them with a `null_resource`
+
+- Reference-[Time provider](https://registry.terraform.io/providers/hashicorp/time/latest/docs)
+    - Time provider is used to intracted with time-based resources the provider itself has no configuration options,
+
+
+## Usecase:
+- Force a resource to update based on a changed null_resource
+- Create `time_sleep` resource to wait for 90 seconds After EC2 instance crreation 
+- create Null Resource with required ptovisioners [Null Resource with required ptovisioners](https://github.com/sudheermuthyala/terraform-/blob/main/08-terraform-provisioners/04-null-resoure/108-null-resource-intro/c3-ec2-instance.tf)
+    - **File provisioner:** copy Application/cart-app Folder to /tmp
+    - **Remote_exec Provisioner:**  Copy Cart-app folder from /tmp to /var/www/html
+## Over the process we will learn about
+- null_resource
+- time_sleep resource
+- we will also learn how to force the resource to update based on a changed null_ressource using `timestamp function` and `triggers `in `null resource`
+- How you write the logic without impacting the existing resources is important thing, at that poin of time Null resource is will be a helpfull thing for you 
+- Hear we are 
+
+
---
+++ 08-terraform-provisioners/04-null-resoure/109-/c3-ec2-instance.tf
@@ -0,0 +1,86 @@
+# Resource aws ec2_instance terraform
+resource "aws_instance" "MY_ec2_VM" {
+  #count                  = terraform.workspace == "default" ? 2 : 1 ## this is a condition i am writing for to create instance according to the workspace
+  ami                    = data.aws_ami.amzlinux.id
+  instance_type          = var.ec2_instance_type
+  key_name               = "terraform"
+  #count = var.ec2_count
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.vpc-web.id]
+  user_data = file("apache-install.sh")
+  # user_data              = <<-EOF
+  #   #! /bin/bash
+  #   sudo yum update -y
+  #   sudo yum install -y httpd
+  #   sudo systemctl  start  httpd
+  #   sudo systemctl enable httpd
+  #   echo "<h1>Welcome to DevOps World ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+  #   EOF
+
+  tags = {
+    "Name" = "vm-${terraform.workspace}"
+    # "demotag"  = "refreshtags"
+    # "target" = "Target-Test-1"   // Adding New Tags for -target
+
+  }
+}
+
+## Wait for 90 seconds after creating the Above EC2 Instance
+resource "time_sleep" "Wait_90_seconds" {
+  depends_on = [ aws_instance.MY_ec2_VM  ]
+  create_duration = "90s"
+}
+
+resource "null_resource" "sync_application_static" {
+  depends_on = [ time_sleep.Wait_90_seconds  ]
+  triggers = {
+    always-update = timestamp()
+  }
+
+# connection Block for provisioners to connect to EC2 Instance
+  connection {
+    type        = "ssh"
+     #host        = self.public_ip   # Understand What is a Public IP
+     host        = aws_instance.MY_ec2_VM.public_ip  # Understand What is a Public IP
+     user        = "ec2-user"
+     password    = ""
+     private_key = file("private-key/terraform.pem")
+ 
+  }
+# Copy the cart-app folder to /tmp
+  provisioner "file" {
+    source =  "Applications/cart-app"
+    destination = "/tmp"
+
+  }
+  # copy the /tmp/cart-app folder to Apache webserer /var/www/html
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cp -r /tmp/cart-app/ /var/www/html/cart-app"
+    ]
+  
+  }
+  
+}
+
+  
+#   # Connection Block for Provisioners to connect to EC2 Instance
+#   connection {
+#     type        = "ssh"
+#     host        = self.public_ip   # Understand What is a Public IP
+#     user        = "ec2-user"
+#     password    = ""
+#     private_key = file("private-key/terraform.pem")
+#   }
+
+#   provisioner "local-exec" {
+#     command = "echo ${aws_instance.MY_ec2_VM.public_ip} >>creation-time-output-ip.txt"
+#     working_dir = "local-exec-output-files/"
+#   }
+
+#   provisioner "local-exec" {
+#     when = destroy
+#     command = "echo Destroy Time provisioner Instance Destroyed at `date` >> destroy-time-output.txt"
+#     working_dir = "local-exec-output-files/"
+#   }
+# }
+
