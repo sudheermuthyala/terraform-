857af6a9f97e4afcac782db0af89e0ec9753499d
sudheer <sudheerlikeu@gmail.com>
2021-12-26 12:34:25 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/04-null-resoure/108-null-resource-intro/c3-ec2-instance.tf
+++ 08-terraform-provisioners/04-null-resoure/108-null-resource-intro/c3-ec2-instance.tf
@@ -52,7 +52,7 @@ resource "null_resource" "sync_application_static" {
     destination = "/tmp"
 
   }
-  # copy the /tmp/cart-app folder
+  # copy the /tmp/cart-app folder to Apache
   provisioner "remote-exec" {
     inline = [
       "sudo cp -r /tmp/cart-app/ /var/www/html"
