8dce1c4aba30e21324d7470fb04fb5b3e5129325
sudheer <sudheerlikeu@gmail.com>
2021-12-26 12:29:44 +0530

    --- Auto-Git Commit ---

--- 08-terraform-provisioners/04-null-resoure/108-null-resource-intro/c3-ec2-instance.tf
+++ 08-terraform-provisioners/04-null-resoure/108-null-resource-intro/c3-ec2-instance.tf
@@ -36,7 +36,7 @@ resource "null_resource" "sync_application_static" {
     always-update = timestamp()
   }
 
-# connection Block for 
+# connection Block for provisioners to connect to 
   connection {
     type        = "ssh"
      #host        = self.public_ip   # Understand What is a Public IP
