9d431cba315856fa502d7a66efe686583735a6a0
sudheer <sudheerlikeu@gmail.com>
2021-12-29 08:39:01 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c4-ec2-instance-modulet.tf
+++ 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c4-ec2-instance-modulet.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "terraform"
   monitoring             = true
-  vpc_security_group_ids = ["sg-05a9b3b6d33836a61"] 
+  vpc_security_group_ids = ["sg-05a9b3b6d33836a61"] ## Get Default 
   subnet_id              = "subnet-eddcdzz4"
 
   tags = {
