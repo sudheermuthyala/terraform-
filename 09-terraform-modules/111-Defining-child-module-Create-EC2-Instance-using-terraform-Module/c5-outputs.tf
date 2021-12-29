4398caf70bd9a6e02b7fabd17dbe9c13b23f1a49
sudheer <sudheerlikeu@gmail.com>
2021-12-29 10:43:41 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c5-outputs.tf
+++ 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c5-outputs.tf
@@ -1,9 +1,13 @@
 output "EC2_Instance_Public_ip" {
     description = "EC2_Instance_Public_ip"
-    value = module.ec2_instance.public_ip
+    value = module.ec2_instance.*.public_ip
 }
 
 output "EC2_Instance_Private_ip" {
     description = "EC2_Instance_Private_ip"
     value = module.ec2_instance.*.private_ip
+}
+
+output "name" {
+    value = ""
 }
