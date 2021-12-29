effc86d24e4ae6b10b60f0fe9c8db1086ccf1142
sudheer <sudheerlikeu@gmail.com>
2021-12-29 08:16:24 +0530

    --- Auto-Git Commit ---

---
+++ 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c1-versions.tf
@@ -0,0 +1,37 @@
+# treeaform Block
+terraform {
+  required_version = "~> 1.1.1"
+  required_providers {
+    aws = {
+        version = "~> 3.0"
+        source = "hashicorp/aws"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = "~> 0.7.0"
+    }
+  }
+  #Adding Backend as S3 for Remote State Storage with State Locking
+  
+  # backend "s3" {
+  #   bucket = "terraformsud"
+  #   key    = "qa/terraform.tfstate"
+  #   region = "us-east-1"
+
+  #   # For State Locking in Dynamodb_Table
+  #   dynamodb_table = "terraformdheeropsdev"
+
+  
+  # }
+}
+
+#provider block
+
+provider "aws" {
+  profile = "default"
+  region =  "us-east-1"
+}
