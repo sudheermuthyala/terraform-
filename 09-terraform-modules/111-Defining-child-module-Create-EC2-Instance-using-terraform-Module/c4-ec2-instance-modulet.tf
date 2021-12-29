ccf6670a4485c799d59781fde838eab729210d30
sudheer <sudheerlikeu@gmail.com>
2021-12-29 08:33:19 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c4-ec2-instance-modulet.tf
+++ 09-terraform-modules/111-Defining-child-module-Create-EC2-Instance-using-terraform-Module/c4-ec2-instance-modulet.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  name = "Demo"
 
   ami                    = "ami-ebd02392"
   instance_type          = "t2.micro"
