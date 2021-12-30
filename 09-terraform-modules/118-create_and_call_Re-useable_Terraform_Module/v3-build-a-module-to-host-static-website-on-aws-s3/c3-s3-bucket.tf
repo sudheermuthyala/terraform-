962deabeb4ff57721093437b74c82e5065b9fddc
sudheer <sudheerlikeu@gmail.com>
2021-12-30 12:56:00 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c3-s3-bucket.tf
+++ 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c3-s3-bucket.tf
@@ -3,6 +3,6 @@
 module "mY_s3_bucket" {
     source = "./modules/aws_s3_static_website_bucket" ## Mandatory
     #version = "value" ## optional
-    bucket_name = var.bucket_name
+    bucket_name = var.my_s3_bucket_name
     tags    = var.s3_tags
 }
