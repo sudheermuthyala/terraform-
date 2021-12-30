df90c82cf6ba6ce5e65a320805bfeef185f4cf24
sudheer <sudheerlikeu@gmail.com>
2021-12-30 12:18:11 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c1-versions.tf
+++ 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c1-versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  
+  required_version = "~> 1.1.1"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
 }
