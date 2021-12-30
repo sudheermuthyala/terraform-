4df7650ff83ee824d00e201293749039621342dc
sudheer <sudheerlikeu@gmail.com>
2021-12-30 13:15:04 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c2-variables.tf
+++ 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c2-variables.tf
@@ -8,7 +8,7 @@ variable "aws_reagion" {
 variable "my_s3_bucket_name" {
     description = "Name for s3-bucket"
     type = string
-    default = "mybucket_19121988"
+    default = "mybucket19121988"
 }
 
 variable "my_s3_tags" {
