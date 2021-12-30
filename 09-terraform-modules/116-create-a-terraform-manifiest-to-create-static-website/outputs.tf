fba1ddf28615c326085d202c06bc5f66177cf63e
sudheer <sudheerlikeu@gmail.com>
2021-12-30 08:06:37 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/116-create-a-terraform-manifiest-to-create-static-website/outputs.tf
+++ 09-terraform-modules/116-create-a-terraform-manifiest-to-create-static-website/outputs.tf
@@ -21,5 +21,5 @@ output "domain" {
 ## Website Endoint of bucket
 output "endpoint" {
   description = "Endpoint of the bucket"
-   value = aws_s3_bucket.s3_bucket.
+  value = aws_s3_bucket.s3_bucket.website_endpoint
 }
