beea4e26243979cdbe8553003ec5a56245a5bc3c
sudheer <sudheerlikeu@gmail.com>
2021-12-30 13:01:21 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c4-outputs.tf
+++ 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c4-outputs.tf
@@ -5,7 +5,7 @@ output "website_bucket_arn" {
   value = module.mY_s3_bucket.arn
 }
 ## S3 Bucket Name
-output "website_bucket_arn" {
+output "website_bucket_name" {
   value = module.mY_s3_bucket.arn
 }
 ## S3 Bucket Domain
