e1026a90e17cc8064646d56fd74a34d3026554e1
sudheer <sudheerlikeu@gmail.com>
2021-12-30 12:20:21 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c2-variables.tf
+++ 09-terraform-modules/118-create_and_call_Re-useable_Terraform_Module/v3-build-a-module-to-host-static-website-on-aws-s3/c2-variables.tf
@@ -1,6 +1,9 @@
 variable "aws_reagion" {
     description = "Reagion In which AWS Resource to be created"
     default = "us-east-1"
-    type = string 
+    type = string   
+}
+
+variable "" {
   
 }
