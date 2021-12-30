23ee3a84b7ce05eee42d1337c8e4f0ac5a2f6b40
sudheer <sudheerlikeu@gmail.com>
2021-12-30 07:56:35 +0530

    --- Auto-Git Commit ---

--- 09-terraform-modules/116-create-a-terraform-manifiest-to-create-static-website/main.tf
+++ 09-terraform-modules/116-create-a-terraform-manifiest-to-create-static-website/main.tf
@@ -3,7 +3,7 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   arn = "public-read"
-  tags = var
+  tags = var.tags
   #policy = file(policy-public-read-access-for-website.json) 
   policy = <<EOF
 {
