0bdc13d2f6ae76676d9dc08704edd53225836c6f
sudheer <sudheerlikeu@gmail.com>
2022-03-13 16:39:15 +0530

    --- Auto-Git Commit ---

--- 03-terraform-resources/44-create-ec2-instance-resource-using-terraform-01-depends_on/c3-ec2-instance.tf
+++ 03-terraform-resources/44-create-ec2-instance-resource-using-terraform-01-depends_on/c3-ec2-instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "MY_DEV_VM" {
     key_name = "terraform-k"
     subnet_id = aws_subnet.DEV_SUBNET.id
     vpc_security_group_ids = [ aws_security_group.DEV_PUB_SG.id ]
-    user_data = file("apache-install.sh")    ## file(function) used 
+    #user_data = file("apache-install.sh")    ## file(function) used 
     # user_data = <<-EOF
     # #! /bin/bash
     # sudo yum update -y
